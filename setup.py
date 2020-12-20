df283239dea7068375851094975648e56fbd6db9
drdr xp <drdr.xp@gmail.com>
2020-12-20 11:49:20 +0800

    release: v0.1.1

---
+++ setup.py
@@ -0,0 +1,23 @@
+# DO NOT EDIT!!! built with `python _building/build_setup.py`
+import setuptools
+setuptools.setup(
+    name="k3down2",
+    packages=["k3down2"],
+    version="0.1.1",
+    license='MIT',
+    description='convert markdown segment into easy to transfer media sucha images.',
+    long_description='# k3down2\n\n[![Build Status](https://travis-ci.com/pykit3/k3down2.svg?branch=master)](https://travis-ci.com/pykit3/k3down2)\n![Python package](https://github.com/pykit3/k3down2/workflows/Python%20package/badge.svg)\n[![Documentation Status](https://readthedocs.org/projects/k3down2/badge/?version=stable)](https://k3down2.readthedocs.io/en/stable/?badge=stable)\n[![Package](https://img.shields.io/pypi/pyversions/k3down2)](https://pypi.org/project/k3down2)\n\nconvert markdown segment into easy to transfer media sucha images.\n\nk3down2 is a component of [pykit3] project: a python3 toolkit set.\n\n\nk3down2 is utility to convert markdown segment into easy to transfer media sucha images.\n\n\n\n\n# Install\n\n```\npip install k3down2\n```\n\n# Synopsis\n\n```python\n\n```\n\n#   Author\n\nZhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n#   Copyright and License\n\nThe MIT License (MIT)\n\nCopyright (c) 2015 Zhang Yanpo (张炎泼) <drdr.xp@gmail.com>\n\n\n[pykit3]: https://github.com/pykit3',
+    long_description_content_type="text/markdown",
+    author='Zhang Yanpo',
+    author_email='drdr.xp@gmail.com',
+    url='https://github.com/pykit3/k3down2',
+    keywords=['markdown', 'python', 'tex'],
+    python_requires='>=3.0',
+
+    install_requires=['semantic_version~=2.8.5', 'jinja2~=2.11.2', 'PyYAML~=5.3.1', 'sphinx~=3.3.1', 'k3ut~=0.1.7', 'k3proc~=0.2.10'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+    ] + ['Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7', 'Programming Language :: Python :: 3.8', 'Programming Language :: Python :: Implementation :: PyPy'],
+)
